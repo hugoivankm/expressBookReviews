e68851ca28e6049eae3d4fee7a3195f010e8c465
hugoivankm <hugoivank@gmail.com>
2023-10-14 20:34:04 -0600

    Fixed shadowed variable naming in is PropertyInBooks predicate function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,18 +5,17 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 const isPropertyInBooks = (property, _value, _ISBN) => {
-    return (value, ISBN) => {
-      let contextProperty = property;
-      if (contextProperty && ISBN) {
-        return books[ISBN][contextProperty].toLowerCase() === value.toLowerCase();
-      }
-      return false;
+  return (value, ISBN) => {
+    let contextProperty = property;
+    if (contextProperty && ISBN) {
+      return books[ISBN][contextProperty].toLowerCase() === value.toLowerCase();
     }
+    return false;
+  }
 }
 
 public_users.post("/register", (_req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  
 });
 
 
@@ -48,7 +47,6 @@ public_users.get('/author/:author', function (req, res) {
     if (isAuthorOfBook(author, ISBN)) {
       booksFromAuthor[ISBN] = books[ISBN];
     }
-
   }
   return res.json(booksFromAuthor);
 });
@@ -60,7 +58,7 @@ public_users.get('/title/:title', function (req, res) {
   const booksWithTitle = {};
   const isTitleOfBook = isPropertyInBooks("title");
 
-  for (const ISBN in books) {
+  for (let ISBN in books) {
     if (isTitleOfBook(title, ISBN)) {
       booksWithTitle[ISBN] = books[ISBN];
     }
@@ -69,9 +67,18 @@ public_users.get('/title/:title', function (req, res) {
 });
 
 //  Get book review
-public_users.get('/review/:isbn', function (_req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  const bookReviews = {};
+
+  for(let ISBN in books){
+    if (ISBN === isbn){
+      bookReviews[ISBN] = books[isbn].reviews;
+    }
+  }
+
+  res.send(bookReviews);
+
 });
 
 module.exports.general = public_users;
