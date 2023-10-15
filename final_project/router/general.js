c12f55bcfec75e3d546d5284ba385e53aa04f4d9
hugoivankm <hugoivank@gmail.com>
2023-10-14 20:05:47 -0600

    Implement get book by title and refactored property in books function

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,14 +4,24 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const isPropertyInBooks = (property, _value, _ISBN) => {
+    return (value, ISBN) => {
+      let contextProperty = property;
+      if (contextProperty && ISBN) {
+        return books[ISBN][contextProperty].toLowerCase() === value.toLowerCase();
+      }
+      return false;
+    }
+}
 
-public_users.post("/register", (req, res) => {
+public_users.post("/register", (_req, res) => {
   //Write your code here
   return res.status(300).json({ message: "Yet to be implemented" });
 });
 
+
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
+public_users.get('/', function (_req, res) {
   return res.send(JSON.stringify(books, null, 4));
 });
 
@@ -24,36 +34,42 @@ public_users.get('/isbn/:isbn', function (req, res) {
   } else {
     return res.status(404).send("ISBN not found");
   }
+
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
   const booksFromAuthor = {};
-  
-  function authorInBooks(author, ISBN){
-    return books[ISBN].author.toLowerCase() === author.toLowerCase();
-  }
+  const isAuthorOfBook = isPropertyInBooks("author");
 
-  for (const ISBN in books){
-    if(authorInBooks(author, ISBN)){
-      booksFromAuthor[ISBN] = books[ISBN]
+  for (const ISBN in books) {
+    console.log(isAuthorOfBook(author, ISBN));
+    if (isAuthorOfBook(author, ISBN)) {
+      booksFromAuthor[ISBN] = books[ISBN];
     }
 
   }
-
   return res.json(booksFromAuthor);
 });
 
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const title = req.params.title;
+  const booksWithTitle = {};
+  const isTitleOfBook = isPropertyInBooks("title");
+
+  for (const ISBN in books) {
+    if (isTitleOfBook(title, ISBN)) {
+      booksWithTitle[ISBN] = books[ISBN];
+    }
+  }
+  return res.json(booksWithTitle);
 });
 
 //  Get book review
-public_users.get('/review/:isbn', function (req, res) {
+public_users.get('/review/:isbn', function (_req, res) {
   //Write your code here
   return res.status(300).json({ message: "Yet to be implemented" });
 });
